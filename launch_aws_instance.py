c437b689c2991f904abe3e63c02195d225256e20
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-10-26 16:43:19 -0400

    Added comments to the front end files

--- launch_aws_instance.py
+++ launch_aws_instance.py
@@ -1,8 +1,5 @@
 import boto.ec2
 
-conn = boto.ec2.connet_to_region ('us-east-1')
-
+conn = boto.ec2.connect_to_region('us-east-1')
 
 resp = conn.run_instances('ami-88aa1ce0', instance_type='t1.micro', key_name='joogle_key', security_groups=['csc326-group5'])
-# instance_id = resp['Instances'][0]['InstanceId']
-# ec2.start_instances(InstanceIds=[instance_id])
