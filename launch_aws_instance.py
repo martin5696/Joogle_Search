9185f72332c10002d481a71da87192d1418b086d
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-10-26 16:47:49 -0400

    added comments in launch_aws_instance.py

--- launch_aws_instance.py
+++ launch_aws_instance.py
@@ -1,5 +1,7 @@
 import boto.ec2
 
+# establisth connection with region us-east-1
 conn = boto.ec2.connect_to_region('us-east-1')
 
+# run aws instance with key_name='joogle_key' and security_groups=['csc326-group5']
 resp = conn.run_instances('ami-88aa1ce0', instance_type='t1.micro', key_name='joogle_key', security_groups=['csc326-group5'])
