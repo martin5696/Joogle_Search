6218d4b45572b375f9e806f0cfd9e8e0081c7b46
Jorge Ramos <jorge.ramos@mail.utoronto.ca>
2018-11-02 15:12:48 -0400

    added some pagination stuff for Lab3 front end

--- joogle_search.py
+++ joogle_search.py
@@ -38,6 +38,48 @@ search_history_path="search_history/"
 
 recent_history_path="recent_history/"
 
+retrieved_list_of_urls = {
+  'current_page_num': 0,
+  'total_page_num': 5,
+  'url_results_info': [
+    [
+      {'url': 'https://www.google.ca', 'pagerank': 25, 'description': 'this is description 25!'},
+      {'url': 'https://www.google.ca', 'pagerank': 24, 'description': 'this is description 24!'},
+      {'url': 'https://www.google.ca', 'pagerank': 23, 'description': 'this is description 23!'},
+      {'url': 'https://www.google.ca', 'pagerank': 22, 'description': 'this is description 22!'},
+      {'url': 'https://www.google.ca', 'pagerank': 21, 'description': 'this is description 21!'}
+    ],
+    [
+      {'url': 'https://www.google.ca', 'pagerank': 20, 'description': 'this is description 20!'},
+      {'url': 'https://www.google.ca', 'pagerank': 19, 'description': 'this is description 19!'},
+      {'url': 'https://www.google.ca', 'pagerank': 18, 'description': 'this is description 18!'},
+      {'url': 'https://www.google.ca', 'pagerank': 17, 'description': 'this is description 17!'},
+      {'url': 'https://www.google.ca', 'pagerank': 16, 'description': 'this is description 16!'}
+    ],
+    [
+      {'url': 'https://www.google.ca', 'pagerank': 15, 'description': 'this is description 15!'},
+      {'url': 'https://www.google.ca', 'pagerank': 14, 'description': 'this is description 14!'},
+      {'url': 'https://www.google.ca', 'pagerank': 13, 'description': 'this is description 13!'},
+      {'url': 'https://www.google.ca', 'pagerank': 12, 'description': 'this is description 12!'},
+      {'url': 'https://www.google.ca', 'pagerank': 11, 'description': 'this is description 11!'}
+    ],
+    [
+      {'url': 'https://www.google.ca', 'pagerank': 10, 'description': 'this is description 10!'},
+      {'url': 'https://www.google.ca', 'pagerank': 9, 'description': 'this is description 9!'},
+      {'url': 'https://www.google.ca', 'pagerank': 8, 'description': 'this is description 8!'},
+      {'url': 'https://www.google.ca', 'pagerank': 7, 'description': 'this is description 7!'},
+      {'url': 'https://www.google.ca', 'pagerank': 6, 'description': 'this is description 6!'}
+    ],
+    [
+      {'url': 'https://www.google.ca', 'pagerank': 5, 'description': 'this is description 5!'},
+      {'url': 'https://www.google.ca', 'pagerank': 4, 'description': 'this is description 4!'},
+      {'url': 'https://www.google.ca', 'pagerank': 3, 'description': 'this is description 3!'},
+      {'url': 'https://www.google.ca', 'pagerank': 2, 'description': 'this is description 2!'},
+      {'url': 'https://www.google.ca', 'pagerank': 1, 'description': 'this is description 1!'}
+    ]
+  ]
+}
+
 # callback function for GET http request. It returns template/homepage.tpl as the view for the root page
 @get('/')
 def search():
@@ -129,40 +171,50 @@ def signout():
 # callback function for POST http request. It's the result page after form submission and returns template/resultpage.tpl
 @post('/results')
 def do_search():
-    global recent_search_history
-    # get the raw input from user using an html form
-    keywords = request.forms.get('keywords')
+  global recent_search_history
+  # get the raw input from user using an html form
+  keywords = request.forms.get('keywords')
 
-    # parse words to remove special characters and split into a list of individual words
-    words = parseQueryIntoWordList(keywords)
+  # parse words to remove special characters and split into a list of individual words
+  words = parseQueryIntoWordList(keywords)
 
-    # populate query_word_occurence
-    query_word_occurence = findWordOccurenceInQuery(words)
-    
-    session = request.environ.get('beaker.session')
+  # populate query_word_occurence
+  query_word_occurence = findWordOccurenceInQuery(words)
+  
+  session = request.environ.get('beaker.session')
 
-    # if user logged in, display search history and store current search results
-    if session['logged_in'] == True:
-      # input words from current search into the global variable word_occurence_history
-      inputWordsInOccurrenceHistory(query_word_occurence)
+  # if user logged in, display search history and store current search results
+  if session['logged_in'] == True:
+    # input words from current search into the global variable word_occurence_history
+    inputWordsInOccurrenceHistory(query_word_occurence)
 
-      # input words from current search into the global variable recent_search_history
-      # this function also removes duplicates from recent_search_history
-      inputWordsInRecentHistory(words)
+    # input words from current search into the global variable recent_search_history
+    # this function also removes duplicates from recent_search_history
+    inputWordsInRecentHistory(words)
 
-      #get 10 words from recent_search_list from global var recent_search_history
-      recent_search_list = recent_search_history[:10]
+    #get 10 words from recent_search_list from global var recent_search_history
+    recent_search_list = recent_search_history[:10]
 
-      # return the top 20 most frequently searched words in word_occurence_history
-      sorted_words = getTop10KeywordsDescending()
-    else:
-      sorted_words = []
-      recent_search_list = []
+    # return the top 20 most frequently searched words in word_occurence_history
+    sorted_words = getTop10KeywordsDescending()
+  else:
+    sorted_words = []
+    recent_search_list = []
 
-    user_info = { 'logged_in': session['logged_in'], 'name': session['name'], 'picture_path': session['picture'] }
+  user_info = { 'logged_in': session['logged_in'], 'name': session['name'], 'picture_path': session['picture'] }
 
-    # use template/resultpage.tmp as the view for the search results page
-    return template('resultpage', user_info = user_info, keywords = keywords, recent_search_list = recent_search_list, sorted_words = sorted_words, query_word_occurence = query_word_occurence)
+  # use template/resultpage.tmp as the view for the search results page
+  return template('resultpage', user_info = user_info, keywords = keywords, recent_search_list = recent_search_list, sorted_words = sorted_words, query_word_occurence = query_word_occurence, retrieved_list_of_urls = retrieved_list_of_urls)
+
+@get('/paginate_results/<page_num>')
+def paginate_results(page_num):
+  global retrieved_list_of_urls
+  retrieved_list_of_urls['current_page_num'] = int(page_num)
+
+  session = request.environ.get('beaker.session')
+  user_info = { 'logged_in': session['logged_in'], 'name': session['name'], 'picture_path': session['picture'] }
+
+  return template('resultpage', user_info = user_info, keywords = 'yolo swag', recent_search_list = [], sorted_words = [], query_word_occurence = [], retrieved_list_of_urls = retrieved_list_of_urls)
 
 
 # parse words to remove special characters and split into a list of individual words
--- views/resultpage.tpl
+++ views/resultpage.tpl
@@ -55,6 +55,34 @@
           </table>
         % end
       </div>
+      <div class="result-page-content">
+        <p>Showing results for "{{ keywords }}"</p>
+        % for result in retrieved_list_of_urls['url_results_info'][retrieved_list_of_urls['current_page_num']]:
+          <div>
+            <div class="result-url-header">
+              <a href="{{ result['url'] }}">
+                <h3 class="url-title">Insert Title here</h3>
+                <br>
+                <cite>{{ result['url'] }}</cite>
+              </a>
+            </div>
+            <div class="result-url-description">
+              <span>{{ result['description'] }}</span>
+            </div>
+          </div>
+        % end
+      </div>
+      <div class="pagination-buttons">
+        <table>
+          <tr>
+          % for page_num in range(0, retrieved_list_of_urls['total_page_num']):
+            <td class="{{ 'cur' if page_num == retrieved_list_of_urls['current_page_num'] else '' }}">
+              <a href="/paginate_results/{{page_num}}">{{ page_num + 1 }}</a>
+            </td>
+          % end
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </html>
