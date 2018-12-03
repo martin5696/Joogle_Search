ae8844a5c7a4c86a4e2eb0c6d9c37ccd0a312615
Martin Zhang <linxuan.zhang@mail.utoronto.ca>
2018-12-03 14:13:43 -0500

    deployment script update

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-# $public_DNS will store DNS of new instance
-# launch_aws.py will have to print the public_DNS to stdout
+#run using sh deployment_script.sh
+#chmod u+x if this doesn't run
+echo "	** Please wait while the aws instance spins up"
+echo "	** This may take close to a minute"
+
+
 # this launches a new instance and gets its DNS
-public_DNS = $(python launch_aws.py)
+# $public_DNS will store DNS of new instance
+
+public_dns=$(python launch_aws_instance.py)
+
+echo $public_dns
 
 #copy joogle_search directory inside our instance
-scp -i key_pair.pem <FILE-PATH> ubuntu@<PUBLIC-IP-ADDRESS>:~/<REMOTE-PATH>
+#scp -i key_pair.pem <FILE-PATH> ubuntu@<PUBLIC-IP-ADDRESS>:~/<REMOTE-PATH>
 
-SCRIPT="
-cd joogle_search;
+#SCRIPT="
+#cd joogle_search;
 
-pip install ...
-...
+# pip install ...
+# ...
 
-./launch_joogle.sh;
+# ./launch_joogle.sh;
 
-exit;
-"
+# exit;
+# "
 
-#go into the instance, install packages, launch our site, then exit.
-ssh -i key_pair.pem ubuntu@<PUBLIC-IP-ADDRESS> "${SCRIPT}"
+# #go into the instance, install packages, launch our site, then exit.
+# ssh -i key_pair.pem ubuntu@<PUBLIC-IP-ADDRESS> "${SCRIPT}"
 
--- launch_aws_instance.py
+++ launch_aws_instance.py
@@ -1,7 +1,20 @@
 import boto.ec2
+import time
+import unicodedata
 
-# establisth connection with region us-east-1
+# Establisth connection with region us-east-1
 conn = boto.ec2.connect_to_region('us-east-1')
 
-# run aws instance with key_name='joogle_key' and security_groups=['csc326-group5']
+# Run aws instance with key_name='joogle_key' and security_groups=['csc326-group5']
 resp = conn.run_instances('ami-88aa1ce0', instance_type='t1.micro', key_name='joogle_key', security_groups=['csc326-group5'])
+
+inst = resp.instances[0]
+
+# Need to insert delay because the instance takes some time to spin up
+time.sleep(70)
+
+inst.update()
+public_dns = inst.public_dns_name
+
+public_dns_ascii = unicodedata.normalize('NFKD', public_dns).encode('ascii', 'ignore')
+print(public_dns_ascii)
