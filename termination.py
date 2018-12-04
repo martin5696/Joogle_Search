a8fd978fcabff34faba1a46292db31ae4f840c50
Martin Zhang <linxuan.zhang@mail.utoronto.ca>
2018-12-03 23:16:53 -0500

    termination script

--- termination.py
+++ termination.py
@@ -3,12 +3,7 @@ import boto.ec2
 # Establisth connection with region us-east-1
 conn = boto.ec2.connect_to_region('us-east-1')
 
-# Run aws instance with key_name='joogle_key' and security_groups=['csc326-group5']
-resp = conn.run_instances('ami-88aa1ce0', instance_type='t1.micro', key_name='martin_key', security_groups=['csc326-group5'])
-
-inst = resp.instances[0]
-
-resp = conn.get_all_reservaions()
+resp = conn.get_all_reservations()
 resp = resp[0]
 inst = resp.instances[0]
 inst.update()
